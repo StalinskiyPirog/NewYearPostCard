3c290821f10d49a1ef3014fd74ce8f17935f6554
StalinskiyPirog <sashuta.zykov@gmail.com>
2022-12-31 04:18:40 +0500

    🎁🎁

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ru">
+      <Head >
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="True"/>
+      <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
